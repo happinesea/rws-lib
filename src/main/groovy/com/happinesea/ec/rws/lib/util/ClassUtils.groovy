abe767fde4f758734188cb7c6e646616c9c2a830
Chunli Hao <hao0323@gmail.com>
2018-03-16 22:12:48 +0900

    add ClassUtils test case.

--- src/main/groovy/com/happinesea/ec/rws/lib/util/ClassUtils.groovy
+++ src/main/groovy/com/happinesea/ec/rws/lib/util/ClassUtils.groovy
@@ -6,13 +6,16 @@ import org.apache.commons.lang.ArrayUtils
 
 import com.happinesea.ec.rws.lib.bean.ApiResponseNode
 
+import groovy.util.logging.Log4j2
+
 /**
  * Class操作関連のUtils
  * 
  * @author loveapple
  *
  */
-public class ClassUtils {
+@Log4j2
+public class ClassUtils extends org.apache.commons.lang.ClassUtils {
 
     /**
      * 親クラスの定義を含めて、{@linkplain ApiResponse}型の{@linkplain Field}を結果に戻す。
@@ -30,18 +33,25 @@ public class ClassUtils {
 	Field[] result = []
 
 	for(Field field : fields) {
-	    if(field.getType().equals(ApiResponseNode.class)) {
-		result[] = field
+	    if(ArrayUtils.contains(field.getType().getInterfaces(), ApiResponseNode.class)) {
+
+		if(log.isDebugEnabled()) {
+		    log.debug('Field type : {} -> field: {} -> interfaces: {}', field.getType(), field.getName(), field.getType().getInterfaces())
+		}
+
+		result = ArrayUtils.add(result, field)
 	    }
 	}
 
 	if(clz.getSuperclass() != null ) {
-	    Field[] tmp = getFields(clz.getSuperclass())
+	    Field[] tmp = getFieldsApiResponse(clz.getSuperclass())
 	    if(!ArrayUtils.isEmpty(tmp)) {
-		ArrayUtils.add(result, tmp)
+		for(Field f : tmp) {
+		    result = ArrayUtils.add(result, f)
+		}
 	    }
 	}
 
-	return fields
+	return result
     }
 }
--- src/test/groovy/com/happinesea/ec/rws/lib/util/ClassUtilsTest.groovy
+++ src/test/groovy/com/happinesea/ec/rws/lib/util/ClassUtilsTest.groovy
@@ -2,8 +2,15 @@ package com.happinesea.ec.rws.lib.util
 import static groovy.test.GroovyAssert.*
 import static org.junit.Assert.*
 
+import java.lang.reflect.Field
+
 import org.apache.commons.lang.ArrayUtils
 import org.junit.Test
+
+import com.happinesea.ec.rws.lib.bean.RwsItemGetResult
+import com.happinesea.ec.rws.lib.bean.RwsItemResponseResult
+import com.happinesea.ec.rws.lib.bean.RwsResponseResult
+import com.happinesea.ec.rws.lib.bean.RwsResponseResult.Status
 
 class ClassUtilsTest {
 
@@ -15,6 +22,21 @@ class ClassUtilsTest {
 
 	assertTrue  ArrayUtils.isEmpty(ClassUtils.getFieldsApiResponse(List.class))
 
-	// TODO go on test code
+	// Test have not superclass
+
+	Field[] fields = ClassUtils.getFieldsApiResponse(RwsResponseResult.class)
+
+	assertEquals 1, fields.length
+	assertEquals 'status', fields[0].getName()
+	assertEquals Status.class, fields[0].getType()
+
+	// Test have superclass
+	fields = ClassUtils.getFieldsApiResponse(RwsItemResponseResult.class)
+	assertEquals 'status', fields[1].getName()
+	assertEquals Status.class, fields[1].getType()
+	assertEquals 'itemGetResult', fields[0].getName()
+	assertEquals RwsItemGetResult.class, fields[0].getType()
+
+	assertEquals 2, fields.length
     }
 }
